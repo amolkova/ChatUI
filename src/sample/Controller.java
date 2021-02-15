8ff70c5c96d34fdcb024c0dd118a88cdb263d96a
amolkova <am7171@mail.ru>
2021-02-15 16:03:56 +0300

    HW-4 done

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,31 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    TextArea historyArea;
+
+    @FXML
+    TextField inputArea;
+
+    @FXML
+    Button sendButton;
+
+    public void btnOneClickAction(ActionEvent actionEvent) {
+        historyArea.appendText(inputArea.getText() + "\n");
+        inputArea.clear();
+        inputArea.requestFocus();
+
+
+
+    }
+
+
+
 }
